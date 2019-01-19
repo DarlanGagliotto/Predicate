8da5b51ef4f0ec9cbb9fa94a152510c5a24933ac
Darlan Gagliotto <dlg_d2@hotmail.com>
2019-01-19 12:24:00 -0200

    Predicate

---
+++ Predicate/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicate.Entities
+{
+   public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public override string ToString() => $"{Name}, {Price.ToString("F2")}";
+
+    }
+}
--- Predicate/Program.cs
+++ Predicate/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Predicate.Entities;
 
 namespace Predicate
 {
@@ -10,8 +8,18 @@ namespace Predicate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.00));
+
+            list.RemoveAll(ProductTest);
+
+            foreach (Product prod in list)
+                Console.WriteLine(prod);
         }
+        public static bool ProductTest(Product p) => p.Price >= 100.00;
     }
 }
